c771ab545134d79375ced0e1e935ce6566483a6c
Cristian Ion <cristian.ion@vivre.eu>
2019-04-05 12:08:38 +0300

    more menu +added db,db connection classes

---
+++ STf/Assets/MenuScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+    public void newGame()
+    {
+    }
+    public void loadGame()
+    {
+
+    }
+    public void quitGame()
+    {
+        Debug.Log("wtf");
+        Application.Quit();
+    }
+}
---
+++ STf/Assets/SqlOpen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Data;
+using Mono.Data.Sqlite;
+using System.IO;
+
+public class SqlOpen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string connection = "URI=file:" + Application.dataPath + "/My_Database";
+        IDbConnection dbcon = new SqliteConnection(connection);
+        dbcon.Open();
+        Debug.Log(connection);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
